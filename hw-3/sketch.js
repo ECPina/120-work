f2d1341160cd9946c454f25f92a2bfcea83860ce
Erica Pina <pina.ericac@gmail.com>
2019-01-27 16:12:12 -0700

    adding a drawing function

--- hw-3/sketch.js
+++ hw-3/sketch.js
@@ -1,6 +1,17 @@
+// Delcaring the setup function
 function setup () {
 
-  createCanvas (600, 500);
+  // creating a canvas that is 700 px wide and 500px high
+  createCanvas (700, 500);
 
+  // making teh background 'thistle/soft purple'
   background ('thistle')
-}ß
+
+  // declaring the draw function
+function draw () {
+
+  // telling p5 to print the text string "Hello World!"
+  // This is going to be positioned at (x: 20, y:30)
+  text ("Hello World!", 30, 40);
+
+}
