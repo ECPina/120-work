cd8d38a702dc2a3a609e0c968c59a3127b6ec014
Erica Pina <pina.ericac@gmail.com>
2019-01-27 16:43:08 -0700

    hello world not showing up, hoping to fix

--- hw-3/sketch.js
+++ hw-3/sketch.js
@@ -1,17 +1,23 @@
+/*
+Name: sketch.js
+Last Modified: 1/27/2019
+Author: E.C.Pina
+*/
+
 // Delcaring the setup function
 function setup () {
 
   // creating a canvas that is 700 px wide and 500px high
   createCanvas (700, 500);
 
-  // making teh background 'thistle/soft purple'
+  // making the background 'thistle/soft purple'
   background ('thistle')
+}
 
   // declaring the draw function
 function draw () {
 
-  // telling p5 to print the text string "Hello World!"
+  // telling p5 to print the text string as "Hello World!"
   // This is going to be positioned at (x: 20, y:30)
   text ("Hello World!", 30, 40);
-
 }
