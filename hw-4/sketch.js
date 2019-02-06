f4edb965f8eb53b9df866d66a4c7261b1a5a0903
Erica Pina <pina.ericac@gmail.com>
2019-02-06 11:59:06 -0700

    fixing typos

--- hw-4/sketch.js
+++ hw-4/sketch.js
@@ -1,13 +1,13 @@
 /*
 Name: sketch.js
-Last Modified:
+Last Modified:02/06/2019
 Author: E.C.Pina
 */
 function setup () {
-  createCanvas (600, 600);
-  background ('pink')
+  createCanvas ( 600, 600 );
+  background ( '#FFB6C1' );
 }
 
 function draw () {
-  rect(25, 30. 450, 540);
+  rect( 25, 30. 450, 540 );
 }
