68d9878f09bc24589af1f181800162b92ddd6fe9
Erica Pina <pina.ericac@gmail.com>
2019-02-13 10:21:40 -0700

    Testing hw 5

---
+++ hw-5/sketch.js
@@ -0,0 +1,15 @@
+/*
+Name: sketch.js
+Last Modified:02/13/2019
+Author: E.C.Pina
+*/
+
+function setup (){
+  createCanvas (500, 500)
+}
+
+fucntion draw(){
+  background ('thistle');
+  translate (width/2, height/2);
+  ellipse (0, 0, 60);
+}
