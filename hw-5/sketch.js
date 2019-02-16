b197dc8c9cb8314d41e02328333f4bf66fa2c571
Erica Pina <pina.ericac@gmail.com>
2019-02-15 18:41:48 -0700

    built animation

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -1,20 +1,77 @@
 /*
 Name: sketch.js
-Last Modified:02/13/2019
+Last Modified: 02/15/2018
 Author: E.C.Pina
 */
 
-function setup () {
-  createCanvas (windowWidth, 800);
+let angle = 0;
+let x = 0;
+let y = 0;
+
+function setup() {
+  createCanvas (800, 800);
+  angleMode (DEGREES);
+  rectMode (CENTER);
 
 }
 
 function draw() {
-  background ('thistle');
+  background ('#FADBD8');
+  noCursor();
 
-  translate (width/2, height/2);
 
-  // ellipse
-  ellipse (0, 0, 60);
-  
+  push();
+
+  // text
+  textSize (25);
+  fill ('white');
+  text ("time: " + floor(mouseX), 10, 40);
+  text ("space: " + floor(mouseY), 10, 80);
+
+  pop();
+
+
+  push();
+
+  // sun circle
+  translate (width/3, height/3);
+  stroke('#F1C40F');
+  fill ('#F7DC6F');
+  ellipse (0, 0, 100);
+
+  // sun square
+  noStroke();
+  translate (0, 0);
+  rotate (angle);
+  fill ('#FCF3CF');
+  rect (0, 0, 80, 80);
+
+  pop();
+
+
+  push();
+
+  // mountains
+  fill('#76D7C4');
+  noStroke();
+  triangle(0, 800, 300, 800, 0, 600);
+  triangle(200, 800, 700, 800, 400, 600);
+  triangle(500, 800, 800, 800, 800, 400);
+
+  pop();
+
+
+  push();
+
+  //  UFO
+  noStroke();
+  fill('#AEB6BF');
+  ellipse(mouseX, mouseY, 200, 40);
+  ellipse(mouseX, mouseY, 80, 80);
+
+  pop();
+
+
+  angle = angle + 4;
+  x = x+2;
 }
