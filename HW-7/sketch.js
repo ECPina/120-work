dc286ced1d736092784594d9cbd5be6056a4bbfc
Erica Pina <pina.ericac@gmail.com>
2019-03-02 12:09:34 -0700

    Update sketch.js

--- HW-7/sketch.js
+++ HW-7/sketch.js
@@ -2,32 +2,34 @@
 // creates an object named ball
 let ball = {};
 
-// size of ball
+// width of the ball is 40
 ball.width = 40;
 
-// ball center
+// center of ball is at 10,10
 ball.x = 10;
 ball.y = 10;
 
-// Changes the angle the ball moves through the canvas
+// Changes the the ball over time, shifting teh ball over and down by 1.
 ball.delta_x = 1;
 ball.delta_y = 1;
+
+// moves the ball along along the x and y axis by 1
 ball.scale_x = 1;
 ball.scale_y = 1;
 
+// creates the canvas that our ball is animated on
 function setup() {
     createCanvas(windowWidth, 400);
     background(255);
 }
 
-
-
+// draws our ball
 function draw() {
 
     ball.x += ball.delta_x * ball.scale_x;
     ball.y += ball.delta_y * ball.scale_y;
 
-
+// Constrains the ball to stay within the parameters of the canvas, changing direction of the ball when it hits the corners
     if (ball.x >= width || ball.x <= 0) {
         ball.delta_x = -1 * ball.delta_x;
     }
@@ -35,10 +37,11 @@ function draw() {
         ball.delta_y = -1 * ball.delta_y;
     }
 
+// gives color of ball and dictates the dimensions of the ball
     fill(255);
     ellipse(ball.x, ball.y, ball.width, ball.width);
 }
-
+ // changes the direction of the ball when the mouse is pressed, as well as the speed
 function mousePressed() {
     ball.scale_x = map(mouseX, 0, width, 0.5, 10);
     ball.scale_y = map(mouseY, 0, height, 0.5, 10);
