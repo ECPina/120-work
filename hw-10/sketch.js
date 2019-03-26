cc85d235730a15f77c96525079df25c5f53af9d0
Erica Pina <pina.ericac@gmail.com>
2019-03-26 09:56:25 -0600

    Update sketch.js

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -1,19 +1,55 @@
+/*
+Name: sketch.js
+Last Modified: 03/20/2018
+Title: Sleep is for Squares
+Author: E.C.Pina
+*/
+
+
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(windowWidth, windowHeight);
+  frameRate(10);
 }
 
 function draw() {
-  background(50);
+  background('#211830');
 
-  lollipop(100, 100, 50);
-  lollipop(300, 200, 150);
+  // diagonal line of colored boxes
+  noStroke();
+  drawBox(15, 15, 20, 20);
 
+  let padding = 25;
+  let size = 75;
+  let number_of_boxes = 9;
+  let x_spacing = (width-padding*4)/(number_of_boxes-2);
+  let y_spacing = (height-padding*4)/(number_of_boxes-2);
+
+  rectMode(CENTER);
+  for (let i=0; i < number_of_boxes; i++) {
+  drawBox(i*x_spacing+padding, i*y_spacing+padding, size, size);
 }
 
-function lollipop(x, y, candy) {
-  fill(0, 200, 255);
-  rect(x, y, 20, 150);
+  // random squares
+  noFill();
+  stroke('#e60efe');
 
-  fill(255, 0, 200);
-  ellipse(x+10, y, candy, candy);
+  let rect_size = random(10, width-10);
+  rect(height/2, width/2, rect_size, rect_size);
+
+  // quote
+  textSize(50);
+  textFont("Impact");
+  fill(100, 250, random(255));
+  text("SLEEP IS FOR", 700, 50);
+}
+
+function drawBox(x1, y1, x2, y2) {
+  fill(250, 250, random(255));
+  rect(x1, y1, x2, y2);
+
+  fill('#433874');
+  rect(x1+20, y1+20, x2, y2);
+
+  fill('#22eae0');
+  rect(x1+40, y1+40, x2, y2);
 }
