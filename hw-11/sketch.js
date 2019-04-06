c6fc585925ca919b380a33158a91a60422134d69
Erica Pina <pina.ericac@gmail.com>
2019-04-06 16:26:23 -0600

    setting up drawing

---
+++ hw-11/sketch.js
@@ -0,0 +1,14 @@
+
+function setup() {
+  createCanvas(windowWidth, 300);
+  background('black');
+}
+
+function draw() {
+
+  fill('white');
+  rect( 15, 15, 200, 200, 5, 90, 5, 150 );
+
+  fill('black');
+  ellipse(150, 150, 100);
+}
