5a2bc9aca050dfa57b3c0237aadee0d403fae31f
Erica Pina <pina.ericac@gmail.com>
2019-04-10 19:50:09 -0600

    DONE

---
+++ hw-11/README.md
@@ -0,0 +1,12 @@
+# Erica Charmaine Pina
+
+[TRULY GREG SKETCH](https://ecpina.github.io/120-work/hw-11/index.html)
+
+## Week Eight Summary
+I honestly didn't know if I would ever figure this part out. I was almost in tears for most of this project. I got it though. After watching video after video and randomly putting things together, I figure it out (hopefully). It's not perfect, but I'm just shocked that I even got this far. God bless Shiffman & the Coding Train.
+
+
+## Issues
+I don't even know where to start. I cycled through several ideas and everything kept breaking over and over again. Finally, I took a break and watched some Danny Gonzalez and I was struck with and idea. I slaved over the keyboard until FINALLY, text began to move across the screen. I honestly was about to give up but I knew I had to just keep trying.
+
+## Workspace
--- hw-11/sketch.js
+++ hw-11/sketch.js
@@ -8,29 +8,37 @@ Title: Truly Greg
 let gregs = [];
 
 function setup() {
-  createCanvas(900, 900);
-  for (let i = 0; i < 3; i++) {
-    gregs[i] = new Greg(10);
+  createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 40; i++) {
+    let x = 10 + 10 * i;
+    gregs[i] = new Greg();
   }
 }
 
 function draw() {
   background(0);
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < gregs.length; i++) {
     gregs[i].move();
     gregs[i].display();
+
+    push();
+    fill(255);
+    textSize(50);
+    text('THE FASTEST GROWING ARMY', 100, 100);
+    text('ON THE INTERNET', 100, 900);
+    pop();
   }
 }
 
 class Greg {
   constructor(){
-    this.x = 10;
+    this.x = 5;
     this.y = 10;
   }
 
   move() {
-    this.x = this.x + 2;
-    this.y = this.y + 3;
+    this.x = this.x + random(-4, 8);
+    this.y = this.y + random(-4, 8);
   }
 
   display() {
@@ -38,5 +46,4 @@ class Greg {
     textSize(50);
     text('GREG', this.x, this.y);
   }
-
 }
