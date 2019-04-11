9a133db9408e2dcb1090768488a3e2e36b9f0b67
Erica Pina <pina.ericac@gmail.com>
2019-04-10 19:10:04 -0600

    one greg works

--- hw-11/sketch.js
+++ hw-11/sketch.js
@@ -5,33 +5,38 @@ Author: E.C.Pina
 Title: Truly Greg
 */
 
-let greg = [];
+let gregs = [];
 
 function setup() {
   createCanvas(900, 900);
+  for (let i = 0; i < 3; i++) {
+    gregs[i] = new Greg(10);
+  }
 }
 
 function draw() {
   background(0);
-
-  greg.display();
-  greg.move();
+  for (let i = 0; i < 3; i++) {
+    gregs[i].move();
+    gregs[i].display();
+  }
 }
 
 class Greg {
-  constructor(x, y, color);
-  this.posX = x;
-  this.posY = y;
-  this.color = color;
-}
-
-display() {
-  fill(this.color);
-  text('GREG', posX, posY);
-}
-
-move() {
-  posX = posX + 1;
-  posY = posY +1;
+  constructor(){
+    this.x = 10;
+    this.y = 10;
   }
+
+  move() {
+    this.x = this.x + 2;
+    this.y = this.y + 3;
+  }
+
+  display() {
+    fill(255);
+    textSize(50);
+    text('GREG', this.x, this.y);
+  }
+
 }
